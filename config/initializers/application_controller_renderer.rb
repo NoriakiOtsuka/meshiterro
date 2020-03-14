3f1893a6e3c9f1ef8592b8cf8614c3c0faf65b24
NoriakiOtsuka <carly13rae7jepson@gmail.com>
2020-03-14 06:13:32 +0000

    commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '309726902312899bdefe305d54f7ebc63ea7ade4a104dc436f7dfb3e4d1723445cfc2275712af7b7e7edc49defa3164491a387b115f287b18deb36f60ab92127'
